7af746bc534da334e2feef5bafe8c74330ec570b
Eric Sabelhaus <eric@sabel.haus>
2016-06-09 00:42:26 -0400

    change to ping pong gui

--- src/gui/SimpleGui.java
+++ src/gui/SimpleGui.java
@@ -6,65 +6,44 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-// create a slice object to store the color and value
-class Slice {
-   double value;
-   Color color;
-   public Slice(double value, Color color) {  
-      this.value = value;
-      this.color = color;
-   }
-}
-
-// create JComponent to store slices
-class MyComponent extends JComponent {
-   private static final long serialVersionUID = 1L;
-   Slice[] slices = { 
-		   new Slice(5, Color.BLACK), 
-		   new Slice(35, Color.CYAN),
-		   new Slice(20, Color.MAGENTA), 
-		   new Slice(15, Color.YELLOW) 
-   };
-   
-   MyComponent() {}
-   
-   public void paint(Graphics g) {
-      drawPie((Graphics2D) g, getBounds(), slices);
-   }
-   
-   void drawPie(Graphics2D g, Rectangle area, Slice[] slices) {
-      double total = 0.0D;
-      for (int i = 0; i < slices.length; i++) {
-         total += slices[i].value;
-      }
-      double curValue = 0.0D;
-      int startAngle = 0;
-      for (int i = 0; i < slices.length; i++) {
-         startAngle = (int) (curValue * 360 / total);
-         int arcAngle = (int) (slices[i].value * 360 / total);
-         g.setColor(slices[i].color);
-         g.fillArc(area.x, area.y, area.width, area.height, 
-         startAngle, arcAngle);
-         curValue += slices[i].value;
-      }
-   }
-}
-
-public class SimpleGui {
-   public static void main(String[] argv) {
-	  //Create and set up the window.
-      JFrame frame = new JFrame("Pie Chart Example");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setLayout (new BorderLayout ());
-      frame.setPreferredSize(new Dimension(400,400));
-      frame.add(new MyComponent(), BorderLayout.CENTER);
-      
-      //Display the window.
-      frame.pack();
-      frame.setVisible(true);
+public class SimpleGui extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected static JFrame frame;
+    
+	public static void main(String[] argv) {
+	   final JButton ping = new JButton("PING");
+	   
+	   ping.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(frame, "PONG");
+				return;
+			}
+		});
+	   
+	   JMenuBar menuBar = new JMenuBar();
+	   menuBar.add(ping);
+	   
+	   //Create and set up the window.
+       JFrame frame = new JFrame("Ping Pong");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setLayout (new BorderLayout ());
+       frame.add(menuBar, BorderLayout.CENTER);
+       //Display the window.
+       frame.pack();
+       frame.setVisible(true);
    }
 }
